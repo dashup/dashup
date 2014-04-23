cfc77f2c4844ce2f800b13292178bb8372eba6c1
Nico Rehwaldt <git_nikku@nixis.de>
2014-04-24 00:38:14 +0200

    fix(app): remove termination hook

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express'),
-    fs = require('fs'),
     config = require('./config/environment'),
     utils = require('./lib/utils');
 
@@ -9,43 +8,10 @@ require('./config/express')(app, config);
 require('./config/routes')(app);
 
 
-
-function initialize() {
-  
-  function terminator(sig) {
-    if (typeof sig === "string") {
-      console.log('[%s] [server] received %s, terminating', utils.now(), sig);
-      process.exit(1);
-    }
-    console.log('[%s] [server] stopped.', utils.now());
-  }
-
-  function hookTerminationHandlers() {
-    //  Process on exit and signals.
-    process.on('exit', terminator);
-
-    // Removed 'SIGPIPE' from the list - bugz 852598.
-    ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-     'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-    ].forEach(function(element, index, array) {
-        process.on(element, terminator);
-    });
-  }
-
-  if (app.get('env') === 'openshift') {
-    hookTerminationHandlers();
-  }
-}
-
-function bootstrapApplication() {
+function start() {
   app.listen(app.get('port'), app.get('hostname'), function() {
     console.log('[%s] [server] listening at %s:%d', utils.now(), app.get('hostname'), app.get('port'));
   });
 }
 
-function start() {
-  bootstrapApplication();
-}
-
-initialize();
 start();
